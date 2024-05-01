3dbde75dd4343cbef92c3fec2f873923fcdb1e91
Zehan Khan <zkhan@hootboard.com>
2024-05-02 01:20:34 +0530

    add javascript array

---
+++ Array/array.js
@@ -0,0 +1,27 @@
+// declare with new key word
+const arr = new Array();
+console.log(arr);
+
+for (let i = 0; i < 10; i++) {
+  arr[i] = i + 1;
+}
+console.log(arr);
+
+// dynamic array
+const numbers = [1, 2, 3, 4, 5, 6];
+
+console.log(numbers);
+
+// insert value at starting
+const insertIndex = 0;
+const insertValue = 10;
+
+// shifting to right
+for (let i = numbers.length; i > insertIndex; i--) {
+  numbers[i] = numbers[i - 1];
+}
+
+// update the index
+numbers[insertIndex] = insertValue;
+
+console.log(numbers);
