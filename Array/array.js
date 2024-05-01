ce6a4b1d909088fb4683d81c755718bd26eb1442
Zehan Khan <zkhan@hootboard.com>
2024-05-02 01:22:45 +0530

    add matrix  js

--- Array/array.js
+++ Array/array.js
@@ -25,3 +25,17 @@ for (let i = numbers.length; i > insertIndex; i--) {
 numbers[insertIndex] = insertValue;
 
 console.log(numbers);
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    console.log(matrix[i][j]);
+  }
+}
+
+console.log(matrix)
