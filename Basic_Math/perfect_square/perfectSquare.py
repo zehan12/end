47ad4ad34f99279da2f3c6d96b9bf1a3165da3ce
zehan khan <zehan9211@gmail.com>
2024-05-15 12:51:30 +0530

    refactor code

--- Basic_Math/perfect_square/perfectSquare.py
+++ Basic_Math/perfect_square/perfectSquare.py
@@ -1,11 +1,20 @@
 def perfectSquare(n):
+    # Iterate through numbers up to the square root of n
     for i in range(int(n**0.5) + 1):
-        if (i*i == n):
-            return i
-    return -1
+        # Check if i*i equals n
+        if i*i == n:
+            return i  # Return the square root if it's a perfect square
+    return -1  # Return -1 if it's not a perfect square
 
 def perfectSquareList(n):
     return next((i for i in range(int(n**0.5) + 1) if i*i == n), -1)
 
+def perfectSquareP(A):
+    sqrt = int(A ** 0.5)  # Calculate the square root of A
+    if (sqrt * sqrt )== A:  # Check if the square of the square root equals A
+        return sqrt  # Return the square root if it's a perfect square
+    return 1  # Return 1 if it's not a perfect square
+
 print(perfectSquare(25))
-print(perfectSquareList(4))
+print(perfectSquareList(25))
+print(perfectSquareP(25))
