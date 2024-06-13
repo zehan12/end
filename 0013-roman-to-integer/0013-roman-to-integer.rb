dc06d054a491a5e9394ea5c2ba6e589a813cc5bf
Zehan Khan <zehan9211@gmail.com>
2024-06-13 14:08:18 +0530

    Time: 72 ms (87.70%), Space: 211.4 MB (47.54%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.rb
@@ -0,0 +1,26 @@
+def roman_to_int(s)
+  # Mapping of Roman numerals to their integer values
+  values = {'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000}
+  
+  # Initialize the total sum
+  sum = 0
+  
+  # Initialize the value of the first Roman numeral
+  prev_value = values[s[0]]
+  
+  # Iterate through each subsequent character in the input string
+  (1...s.length).each do |i|
+    # Get the integer value of the current Roman numeral
+    current_value = values[s[i]]
+    
+    # If the current value is greater than the previous value, subtract the previous value
+    # Otherwise, add the previous value
+    sum += current_value > prev_value ? -prev_value : prev_value
+    
+    # Update the previous value for the next iteration
+    prev_value = current_value
+  end
+  
+  # Add the last value to sum
+  sum + prev_value
+end
