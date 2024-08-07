d4aba81bf6659dad9701bcd9344c82ae0638fe4c
Zehan Khan <zehan9211@gmail.com>
2024-08-07 09:50:00 +0530

    Time: 97 ms (41.34%), Space: 49.5 MB (79.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+// O(n*m) => O(n*2)
 var twoSum = function(nums, target) {
     for ( let i = 0 ; i < nums.length ; i++ ) {
         for ( let j = i + 1 ; j < nums.length; j++ ) {
