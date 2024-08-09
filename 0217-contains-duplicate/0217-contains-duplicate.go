1dbc825a01b8f38de5ef237303a3e7674b98ed34
Zehan Khan <zehan9211@gmail.com>
2024-08-09 23:04:23 +0530

    Time: 89 ms (34.63%), Space: 9.6 MB (52.88%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "sort"
+)
+
+func containsDuplicate(nums []int) bool {
+    sort.Ints(nums)
+    
+    for i := 1; i < len(nums); i++ {
+        if nums[i] == nums[i-1] {
+            return true
+        }
+    }
+    
+    return false
+}
