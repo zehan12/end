3091317051809940cca6c497c73a60866fe88354
Zehan Khan <zehan9211@gmail.com>
2024-12-25 00:42:30 +0530

    Time: 66 ms (43.68%), Space: 50.2 MB (58.2%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,19 @@
+function compose(functions) {
+    
+    return function(x) {
+        
+        if (functions.length === 0) return x;
+    
+        let result = x;
+        
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+        return result;    
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
